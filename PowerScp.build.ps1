64f66ad7b4103d6d59a573f4035f91a8e8487780
Steffen Kampmann <s.kampmann@tekaris.com>
2019-03-03 09:27:00 +0100

    Changed formatting

--- PowerScp.build.ps1
+++ PowerScp.build.ps1
@@ -4,9 +4,9 @@ task Build bin
 
 task bin lib, {
     dotnet build
-    Copy-Item .\Source\PowerScp.psd1 -Destination bin/Debug/netstandard2.0/
-    Copy-Item .\lib\winscp\*\lib\netstandard*\WinSCPnet.dll -Destination bin/Debug/netstandard2.0/
-    Copy-Item .\lib\newtonsoft.json\*\lib\netstandard2.0\Newtonsoft.Json.dll -Destination bin/Debug/netstandard2.0/
+    Copy-Item .\Source\PowerScp.psd1                                            -Destination bin/Debug/netstandard2.0/
+    Copy-Item .\lib\winscp\*\lib\netstandard*\WinSCPnet.dll                     -Destination bin/Debug/netstandard2.0/
+    Copy-Item .\lib\newtonsoft.json\*\lib\netstandard2.0\Newtonsoft.Json.dll    -Destination bin/Debug/netstandard2.0/
 }
 
 task lib {
@@ -14,10 +14,10 @@ task lib {
 }
 
 task Clean {
-    Remove-Item .\bin -Recurse -Force -ErrorAction 'SilentlyContinue' -Verbose
-    Remove-Item .\doc -Recurse -Force -ErrorAction 'SilentlyContinue' -Verbose
-    Remove-Item .\obj -Recurse -Force -ErrorAction 'SilentlyContinue' -Verbose
-    Remove-Item .\lib -Recurse -Force -ErrorAction 'SilentlyContinue' -Verbose
+    Remove-Item .\bin -Recurse -Force -ErrorAction SilentlyContinue -Verbose
+    Remove-Item .\doc -Recurse -Force -ErrorAction SilentlyContinue -Verbose
+    Remove-Item .\obj -Recurse -Force -ErrorAction SilentlyContinue -Verbose
+    Remove-Item .\lib -Recurse -Force -ErrorAction SilentlyContinue -Verbose
 }
 
 task Test {
